93eba38cf048a6b78a7dd4b50eac83bd54f76214
Prakash Kumar <prakash@Prakashs-MacBook-Air.local>
2026-03-21 12:01:21 +0530

    pipeline edited

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import MarketingApp from './components/MarketingApp'
 export default ()=>{
-    return <div><h1>Hello</h1>
+    return <div><h1>Hello kk</h1>
     <hr/>
     <MarketingApp/>
     </div>
