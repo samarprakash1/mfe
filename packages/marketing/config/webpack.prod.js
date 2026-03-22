3e23d07219a5d3300916e67e7b240628f64447a8
Prakash Kumar <prakash@Prakashs-MacBook-Air.local>
2026-03-22 21:38:40 +0530

    config changed

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -2,11 +2,13 @@ const { merge } = require('webpack-merge') // used to merge config
 const ModuleFederation = require('webpack/lib/container/ModuleFederationPlugin')
 const packageJson = require('../package.json')
 const commonConfig = require('./webpack.common')
+const domain=process.env.PRODUCTION_DOMAIN;
+
 const prodConfig = {
     mode: 'production',
     output: {
         filename: '[name].[contenthash].js',
-        publicPath:'/marketing/latest/'
+        publicPath:`${domain}/marketing/latest/`
     },
     plugins: [
         new ModuleFederation({
