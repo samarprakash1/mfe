fff1073e0266029e0e4f17228cfe9c7aeffd8874
Prakash Kumar <prakash@Prakashs-MacBook-Air.local>
2026-03-22 21:43:37 +0530

    config changed

--- packages/auth/config/webpack.prod.js
+++ packages/auth/config/webpack.prod.js
@@ -2,7 +2,7 @@ const { merge } = require('webpack-merge') // used to merge config
 const ModuleFederation = require('webpack/lib/container/ModuleFederationPlugin')
 const packageJson = require('../package.json')
 const commonConfig = require('./webpack.common')
-const domain=process.env.PRODUCTION_DOMAIN;
+const domain=process.env.PRODUCTION_DOMAIN || 'https://dcj5eokbr8w9v.cloudfront.net/';
 
 const prodConfig = {
     mode: 'production',
